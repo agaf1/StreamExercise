cbfb293128c01e62fc70ca31811352913e0b66e1
Agnieszka <agula.filipowicz@gmail.com>
2023-09-06 10:15:07 +0200

    add class Dish

---
+++ src/main/java/pl/aga/Dish.java
@@ -0,0 +1,14 @@
+package pl.aga;
+
+import lombok.Value;
+
+@Value
+public class Dish {
+
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public enum Type{MEAT, FISH, OTHER}
+}
