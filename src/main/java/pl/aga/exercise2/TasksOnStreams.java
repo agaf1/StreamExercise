22ce2f0a2da52e00a51eefb870838e5a860d67d2
Agnieszka <agula.filipowicz@gmail.com>
2023-09-07 09:41:11 +0200

    add method to classes of package exercise2

--- src/main/java/pl/aga/exercise2/TasksOnStreams.java
+++ src/main/java/pl/aga/exercise2/TasksOnStreams.java
@@ -1,4 +1,102 @@
 package pl.aga.exercise2;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class TasksOnStreams {
+
+    public List<Transaction> getSortedTransactionFrom2011(List<Transaction> transactions) {
+        List<Transaction> transactionsFrom2011 =
+                transactions.stream()
+                        .filter(t -> t.getYear() == 2011)
+                        .sorted(Comparator.comparing(Transaction::getValue))
+                        .collect(Collectors.toList());
+
+        return transactionsFrom2011;
+    }
+
+    public Set<String> getAllCity(List<Transaction> transactions) {
+        Set<String> cities =
+                transactions.stream()
+                        .map(t -> t.getTrader().getCity())
+                        .collect(Collectors.toSet());
+
+        return cities;
+    }
+
+    public List<Trader> getSortedTradersFromCambridge(List<Transaction> transactions) {
+        List<Trader> traders =
+                transactions.stream()
+                        .map(Transaction::getTrader)
+                        .filter(d -> d.getCity().equals("Cambridge"))
+                        .distinct()
+                        .sorted(Comparator.comparing(Trader::getName))
+                        .collect(Collectors.toList());
+
+        return traders;
+    }
+
+    public List<String> getSortedNamesOfTraders(List<Transaction> transactions) {
+        List<String> namesOfTraders =
+                transactions.stream()
+                        .map(t -> t.getTrader().getName())
+                        .distinct()
+                        .sorted()
+                        .collect(Collectors.toList());
+
+        return namesOfTraders;
+    }
+
+    public boolean findTraderFromMilan(List<Transaction> transactions) {
+        boolean isTraderFromMilan =
+//                transactions.stream()
+//                        .map(t -> t.getTrader())
+//                        .filter(t -> t.getCity() == "Milan")
+//                        .findAny()
+//                        .isPresent();
+                transactions.stream()
+                        .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
+
+        return isTraderFromMilan;
+    }
+
+    public List<Integer> getAllValueOfTransactionOfTradersFromCambridge(List<Transaction> transactions) {
+        List<Integer> valueOfTransaction =
+                transactions.stream()
+                        .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                        .map(Transaction::getValue)
+                        .collect(Collectors.toList());
+
+        return valueOfTransaction;
+    }
+
+    public int getMaxValueOfTransaction(List<Transaction> transactions) {
+        int maxValue =
+                transactions.stream()
+                        .map(Transaction::getValue)
+                        .reduce(Integer::max)
+                        .orElse(0);
+        return maxValue;
+    }
+
+    public Transaction getTransactionWithMinValue(List<Transaction> transactions) {
+        Transaction minTransaction =
+                transactions.stream()
+//                        .sorted(Comparator.comparing(Transaction::getValue))
+//                        .findFirst()
+//                        .get();
+
+//                        .reduce((t1,t2) -> t1.getValue() < t2.getValue() ? t1 : t2)
+//                        .get();
+
+                        .min(Comparator.comparing(Transaction::getValue))
+                        .get();
+
+        return minTransaction;
+    }
+
+
 }
--- src/test/java/pl/aga/exercise2/TasksOnStreamsTest.java
+++ src/test/java/pl/aga/exercise2/TasksOnStreamsTest.java
@@ -1,13 +1,17 @@
 package pl.aga.exercise2;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TasksOnStreamsTest {
 
-    Trader raoul = new Trader("Raoul","Cambridge");
+    Trader raoul = new Trader("Raoul", "Cambridge");
     Trader mario = new Trader("Mario", "Milan");
     Trader alan = new Trader("Alan", "Cambridge");
     Trader brian = new Trader("Brian", "Cambridge");
@@ -15,12 +19,77 @@ class TasksOnStreamsTest {
     List<Transaction> transactions = Arrays.asList(
             new Transaction(brian, 2011, 300),
             new Transaction(raoul, 2012, 1000),
-            new Transaction(raoul, 2011,400),
-            new Transaction( mario, 2012,710),
+            new Transaction(raoul, 2011, 400),
+            new Transaction(mario, 2012, 710),
             new Transaction(mario, 2012, 700),
-            new Transaction(alan, 2012,950)
+            new Transaction(alan, 2012, 950)
     );
 
     TasksOnStreams tasksOnStreams = new TasksOnStreams();
+
+    @Test
+    public void should_get_sorted_transactions_from_2011() {
+        List<Transaction> result = tasksOnStreams.getSortedTransactionFrom2011(transactions);
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).
+                containsExactlyElementsOf(
+                        List.of(new Transaction(brian, 2011, 300),
+                                new Transaction(raoul, 2011, 400)));
+    }
+
+    @Test
+    public void should_get_All_Cities(){
+        Set<String> result = tasksOnStreams.getAllCity(transactions);
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains("Cambridge","Milan");
+    }
+
+    @Test
+    public void should_get_sorted_Traders_from_Cambridge(){
+        List<Trader> result = tasksOnStreams.getSortedTradersFromCambridge(transactions);
+
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0).getName()).isEqualTo("Alan");
+    }
+
+    @Test
+    public void should_get_sorted_names_of_traders(){
+        List<String> result = tasksOnStreams.getSortedNamesOfTraders(transactions);
+
+        assertThat(result.size()).isEqualTo(4);
+        assertThat(result).containsExactlyElementsOf(List.of("Alan","Brian","Mario","Raoul"));
+    }
+
+    @Test
+    public void should_get_True_when_is_Trader_from_Milan(){
+        boolean result = tasksOnStreams.findTraderFromMilan(transactions);
+
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    public void should_get_Values_Of_Transaction_From_Cambridge(){
+        List<Integer> result = tasksOnStreams.getAllValueOfTransactionOfTradersFromCambridge(transactions);
+
+        System.out.println(result);
+
+        assertThat(result.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void should_get_max_value_from_all_transactions(){
+        int result = tasksOnStreams.getMaxValueOfTransaction(transactions);
+
+        assertThat(result).isEqualTo(1000);
+    }
+
+    @Test
+    public void should_get_min_transaction(){
+        Transaction result = tasksOnStreams.getTransactionWithMinValue(transactions);
+
+        assertThat(result.getValue()).isEqualTo(300);
+    }
 
 }
